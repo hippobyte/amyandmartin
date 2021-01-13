82c17296dd5fe57de4de037789d2cdf3d9e0efa7
Martin Marzejon <martinmarzejon@hippobyte.com>
2021-01-12 21:48:05 -0800

    invitecode

--- src/components/Form/src/InviteCode.js
+++ src/components/Form/src/InviteCode.js
@@ -6,14 +6,26 @@ import { colors } from '../../../style/colors'
 
 const InviteCode = ({ loading, intent, name, maxLength, theme, methods }) => {
   const { control, setValue, watch } = methods
-  const styles = inputTheme(colors, intent, theme)
 
   const onChange = (value) => {
     setValue(name, value)
   }
 
   return (
-    <ThemeContext.Extend value={styles}>
+    <ThemeContext.Extend value={{
+      box: {
+        extend: {
+          '& > div': {
+            width: '100% !important'
+          },
+          '& input': {
+            color: 'black',
+            fontWeight: 900,
+            width: 'calc(100%/8) !important'
+          }
+        }
+      }
+    }}>
       <Box>
         <Controller
           key={name}
@@ -32,30 +44,5 @@ const InviteCode = ({ loading, intent, name, maxLength, theme, methods }) => {
     </ThemeContext.Extend>
   )
 }
-
-export const inputTheme = (colors, intent, theme = {}) => ({
-  box: {
-    extend: {
-      '& input': {
-        color: 'black',
-        fontWeight: 900
-      }
-    }
-  },
-  button: {
-    extend: {
-      position: 'absolute',
-      right: '40px',
-      height: '20px',
-      lineHeight: '14px',
-      fontSize: '14px',
-      fontWeight: '700',
-      padding: '2px 6px',
-      border: `1px solid ${colors["light-7"]}`,
-      borderRadius: '4px',
-      color: `${colors["light-10"]}`
-    }
-  }
-})
 
 export default InviteCode
